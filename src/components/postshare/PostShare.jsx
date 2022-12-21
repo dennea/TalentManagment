a68af02f8019f83eba8f1710bd62d9e6fbd2d006
dennea <dennea1228@gmail.com>
2022-12-20 19:17:42 -0700

    upload image and posts

--- src/components/postshare/PostShare.jsx
+++ src/components/postshare/PostShare.jsx
@@ -6,26 +6,63 @@ import {UilPlayCircle} from "@iconscout/react-unicons"
 import {UilLocationPoint} from "@iconscout/react-unicons"
 import {UilSchedule} from "@iconscout/react-unicons"
 import {UilTimes} from "@iconscout/react-unicons"
+import { useDispatch, useSelector } from 'react-redux'
+import { uploadImage, uploadPost } from '../../actions/UploadAction'
 
 
 const PostShare = () => {
+    const dispatch = useDispatch()
+    const loading = useSelector((state) => state.postReducer.uploading)
     const [image, setImage] = useState(null);
     const imageRef = useRef()
+    const desc = useRef()
+    const {user} = useSelector((state)=>state.authReducer.authData)
 
     const onImageChange =(event)=>{
         if (event.target.files && event.target.files[0]){
             let img = event.target.files[0];
-            setImage({
-                image: URL.createObjectURL(img),
-            });
+            setImage(img);
         }
+    }
+
+    const reset = () => {
+        setImage(null);
+        desc.current.value = ""
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // I think this means to stay on the same page
+
+        const newPost = {
+            userId: user._id, 
+            desc: desc.current.value, 
+        }
+
+        if (image) {
+            const data = new FormData()
+            const filename = Date.now() + image.name
+            data.append("name", filename)
+            data.append("file", image)
+            newPost.image = filename;
+            console.log(newPost)
+
+            try {
+                dispatch(uploadImage(data))
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        dispatch(uploadPost(newPost))
+        reset()
     }
 
     return (
         <div className = "PostShare">
             <img src={ProfileImage} alt='' />
             <div>
-                <input type = "text" placeholder="What's happening" />
+                <input ref = {desc} required type = "text" placeholder="What's happening" />
                 <div className='postOptions'>
                     <div className='option'
                     style = {{color: "var(--photo)"}}
@@ -49,8 +86,10 @@ const PostShare = () => {
                         <UilSchedule />
                         Schedule
                     </div>
-                    <button className='button shareButton'>
-                        Share
+                    <button className='button shareButton'
+                    onClick={handleSubmit}
+                    disabled = {loading}>
+                        {loading? "Uploading":"Share"}
                     </button>
                     <div style = {{display:"none"}}>
                         <input type="file" name="myImage" ref={imageRef} onChange={onImageChange} />
@@ -59,7 +98,7 @@ const PostShare = () => {
                 {image && 
                     <div className="previewImage">
                         <UilTimes onClick={()=>setImage(null)}/>
-                        <img src={image.image} alt="" />
+                        <img src={URL.createObjectURL(image)} alt="" />
                     </div>
                 }
             </div>
